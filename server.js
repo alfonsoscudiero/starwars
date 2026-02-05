bd8ea7a8973cd9c32bcbd07f1b7b54e572d8acbc
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-02-05 10:51:04 -0700

    Initialize Node.js Express project

---
+++ server.js
@@ -0,0 +1,22 @@
+/* ***************************
+ *  Express Web Server
+ * ************************** */// 
+// Load Environment variables
+require("dotenv").config();
+// Express imports
+const express = require('express');
+
+// App creation
+const app = express();
+
+// Server Configuration
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Server Start
+app.listen(PORT, () => {
+  console.log(`[server] Server is running on http://localhost:${PORT}`);
+});
