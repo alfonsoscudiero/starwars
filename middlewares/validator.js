b3a9480e38d29137b90dd5b2447a05c1fdd511f6
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-02-12 13:10:35 -0700

    Add validateParams middleware for route parameter validation

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,7 +1,7 @@
 /* ***************************
  *  middlewares/validator.js
  * ************************** */
-// Higher-order middleware factory for validating request bodies using Joi
+// Middleware factory for validating request bodies using Joi
 const validateBody =
   (schema, options = { abortEarly: false, stripUnknown: true }) =>
   (req, res, next) => {
@@ -19,4 +19,26 @@ const validateBody =
     next();
   };
 
-module.exports = { validateBody };
+// Middleware factory for validating route parameters using Joi
+const validateParams =
+  (schema, options = { abortEarly: false, stripUnknown: true }) =>
+  (req, res, next) => {
+    const { value, error } = schema.validate(req.params, options);
+
+    if (error) {
+      return res.status(422).json({
+        message: 'Invalid route parameter.',
+        details: error.details.map((d) => d.message),
+      });
+    }
+
+    // Assign cleaned params back
+    req.params = value;
+
+    next();
+  };
+
+module.exports = {
+  validateBody,
+  validateParams,
+};
